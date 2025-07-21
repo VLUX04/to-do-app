96cd51f5471b11fb763615e1b1d5f3f0fe83fc59
Leonardo Ribeiro <leonardo13122004@gmail.com>
2025-07-21 01:02:00 +0100

    small progress

--- desktop/backend.js
+++ desktop/backend.js
@@ -15,36 +15,28 @@ function startBackendServer(userDataPath) {
   try {
     const data = fs.readFileSync(todosFilePath, 'utf-8');
     todos = JSON.parse(data);
-    console.log('Loaded todos:', todos);
   } catch (err) {
-    console.log('Starting with empty todo list.', err.message);
   }
 
   function saveTodos() {
     try {
       fs.writeFileSync(todosFilePath, JSON.stringify(todos, null, 2));
-      console.log('Todos saved.');
     } catch (err) {
-      console.error('Failed to save todos:', err);
     }
   }
 
   app.get('/todos', (req, res) => {
-    console.log('GET /todos');
     res.json(todos);
   });
 
   app.post('/todos', (req, res) => {
-    console.log('POST /todos:', req.body);
     const todo = req.body;
     todos.push(todo);
     saveTodos();
     res.status(201).json(todo);
   });
 
-  app.listen(3000, '0.0.0.0', () => {
-    console.log('Backend running on http://localhost:3000');
-  });
+  app.listen(3000, 'localhost');
 }
 
 module.exports = { startBackendServer };
